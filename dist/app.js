896156a15264c284b281c831dc0be2104a7fc8b6
HaiPhu2702 <phukaht@gmail.com>
2022-07-13 21:51:06 +0700

    make it better

--- dist/app.js
+++ dist/app.js
@@ -4,25 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const port = 8080;
+const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
-app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
-app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use(body_parser_1.default.json());
-app.set('view engine', 'ejs');
-app.set("views", path_1.default.join(__dirname, "views"));
-mongoose_1.default.connect(`${process.env.DB_MONGO}`, () => {
+app.set("view engine", "ejs");
+app.set('views', './src/views');
+mongoose_1.default.connect(`${process.env.DB_MONGO}`)
+    .then(() => {
     // tslint:disable-next-line:no-console
-    console.log("connect DB successfully");
+    console.log('DB Connected!');
+})
+    .catch(error => {
+    // tslint:disable-next-line:no-console
+    console.log('DB connection error:', error.message);
 });
-// app.use('/user',userRouter)
+app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
     res.render('./user/register');
 });
-app.listen(port);
+app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log("http://localhost:" + PORT);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,42 @@
 import express from "express";
-import path from "path";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
-
-const port = process.env.PORT||3000;
+import dotenv from "dotenv";
+dotenv.config();
 
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+app.set("view engine", "ejs");
+app.set('views', './src/views');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+
+mongoose.connect(`${process.env.DB_MONGO}`)
+    .then(() =>{
+        // tslint:disable-next-line:no-console
+        console.log('DB Connected!')}
+        )
+    .catch(error =>{
+        // tslint:disable-next-line:no-console
+        console.log('DB connection error:', error.message)
+    }
+       );
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs');
-app.set("views",path.join( __dirname, "views" ));
-
-
- mongoose.connect(`${process.env.DB_MONGO}`,()=>{
-     // tslint:disable-next-line:no-console
-     console.log("connect DB successfully")
- });
-
-
-
-
-
-// app.use('/user',userRouter)
 app.get('/',(req,res)=>{
     res.render('./user/register');
 })
 
 
-app.listen(port)
+app.listen(PORT, () => {
+   // tslint:disable-next-line:no-console
+    console.log("http://localhost:" + PORT)
+})
+
+
+
+
+
 
 
 
