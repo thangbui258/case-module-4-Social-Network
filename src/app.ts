1650fe58d3986ca26555fab7147dbf9484b3438e
HaiPhu2702 <phukaht@gmail.com>
2022-07-13 21:41:22 +0700

    ..

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config()
 
-const port = `${process.env.PORT}`||3000;
+const port = process.env.PORT||3000;
 
 
 const app = express();
