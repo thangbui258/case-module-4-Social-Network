ccda37467c1a21a007027103e34c83ed716ccc14
HaiPhu2702 <phukaht@gmail.com>
2022-07-13 21:31:42 +0700

    make it better

--- dist/app.js
+++ dist/app.js
@@ -6,26 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const port = 8080;
 const app = (0, express_1.default)();
-const port = 8081;
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
 app.set('view engine', 'ejs');
 app.set("views", path_1.default.join(__dirname, "views"));
-// async function main() {
-//     await mongoose.connect('mongodb://localhost:27017/MangXaHoi');
-// }
-// main()
-//     .then(res => {
-//         // tslint:disable-next-line:no-console
-//         console.log('Connected successfully to server');
-//     })
-//     // tslint:disable-next-line:no-console
-//     .catch(console.error)
+mongoose_1.default.connect(`${process.env.DB_MONGO}`, () => {
+    // tslint:disable-next-line:no-console
+    console.log("connect DB successfully");
+});
 // app.use('/user',userRouter)
 app.get('/', (req, res) => {
-    res.render('./layout/layout');
+    res.render('./user/register');
 });
 app.listen(port);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
+
+const port = 8080;
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+app.set("views",path.join( __dirname, "views" ));
+
+
+ mongoose.connect(`${process.env.DB_MONGO}`,()=>{
+     // tslint:disable-next-line:no-console
+     console.log("connect DB successfully")
+ });
+
+
+
+
+
+// app.use('/user',userRouter)
+app.get('/',(req,res)=>{
+    res.render('./user/register');
+})
+
+
+app.listen(port)
+
+
+
